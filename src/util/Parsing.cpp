61ce578891931c68f59c062968c12198d2ab801f
SadSido <sidorian@mail.ru>
2012-11-28 23:06:07 +0400

    Parsing files added to the project

---
+++ src/util/Parsing.cpp
@@ -0,0 +1,33 @@
+// AUTHOR: Wiatcheslav "SadSido" Sidortsov
+// ORIGIN: mischellaneous parsing utilities
+
+#include "Parsing.h"
+
+//************************************************************************************************************************
+
+namespace 
+{
+	CL_String spaces()
+	{ return " \n\r\t";	}
+
+	CL_String digits()
+	{ return "0123456789";	}
+
+	void skipWhile(CL_String::iterator &it, CL_String pattern)
+	{ while (*it && pattern.find(*it)) { ++ it; } }
+
+	void skipUntil(CL_String::iterator &it, CL_String pattern)
+	{ while (*it && !pattern.find(*it)) { ++ it; } }
+}
+
+//************************************************************************************************************************
+
+CL_String parseToken(CL_String::iterator &it)
+{
+	skipWhile(it, spaces());
+	auto beg = it;
+	skipUntil(it, spaces());
+	return CL_String(beg, it - beg);
+}
+
+//************************************************************************************************************************
---
+++ src/util/Parsing.h
@@ -0,0 +1,15 @@
+// AUTHOR: Wiatcheslav "SadSido" Sidortsov
+// ORIGIN: mischellaneous parsing utilities
+
+#ifndef _Parsing_h_
+#define _Parsing_h_
+
+#include <ClanLib/core.h>
+
+//************************************************************************************************************************
+
+CL_String parseToken(CL_String::iterator &it);
+
+//************************************************************************************************************************
+
+#endif
