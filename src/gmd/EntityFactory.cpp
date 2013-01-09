fee20ab597864283a9321f4ac6bf92bcaac808d6
SadSido <sidorian@mail.ru>
2013-01-09 21:51:58 +0400

    EntityFactory files added to project

---
+++ src/gmd/EntityFactory.cpp
@@ -0,0 +1,16 @@
+// AUTHOR: Wiatcheslav "SadSido" Sidortsov
+// ORIGIN: functions to resolve names and types into actual entities
+
+#include "EntityFactory.h"
+#include "EntTest.h"
+
+//************************************************************************************************************************
+
+Entity::Ref createEntity( ... )
+{
+	// assert false;
+	return Entity::Ref();
+}
+
+
+//************************************************************************************************************************
---
+++ src/gmd/EntityFactory.h
@@ -0,0 +1,16 @@
+// AUTHOR: Wiatcheslav "SadSido" Sidortsov
+// ORIGIN: functions to resolve names and types into actual entities
+
+#ifndef _EntityFactory_h_
+#define _EntityFactory_h_
+
+#include <ClanLib/core.h>
+#include "Entities.h"
+
+//************************************************************************************************************************
+
+Entity::Ref createEntity( ... );
+
+//************************************************************************************************************************
+
+#endif
