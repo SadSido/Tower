00080003a9e0e1ec87d714a68635e02b86cfc079
SadSido <sidorian@mail.ru>
2013-05-14 22:44:42 +0400

    Added optional condition to area entity

--- src/ent/AreaEntity.cpp
+++ src/ent/AreaEntity.cpp
@@ -9,6 +9,7 @@
 //************************************************************************************************************************
 	
 AreaEntity::AreaEntity(const CL_DomNodeList &props)
+: m_gen(0), m_cond(true)
 {
 	for (int prNo = 0; prNo < props.get_length(); ++ prNo)
 	{
@@ -20,6 +21,9 @@ AreaEntity::AreaEntity(const CL_DomNodeList &props)
 	
 		if (prop.get_attribute("name") == "entry") 
 		{ m_entry = prop.get_attribute("value"); }
+
+		if (prop.get_attribute("name") == "condition") 
+		{ m_condition = prop.get_attribute("value"); }
 	}
 }
 
@@ -28,7 +32,7 @@ bool AreaEntity::update(const LevelCtx &ctx, float secs)
 	const bool isInside = m_rect.is_inside(ctx.player.getRect());
 	const bool assigned = ctx.player.checkAction(this);
 
-	if (!assigned && isInside)
+	if (!assigned && isInside && checkCondition(ctx))
 	{ ctx.player.setAction(this); }
 
 	else if (assigned && !isInside)
@@ -48,6 +52,19 @@ void AreaEntity::notify(const LevelCtx &ctx, Notify code)
 	assert(code == n_DoAction);
 	GameScene::Ref dlgScene(new AreaScene(ctx.manager, m_area, m_entry));
 	ctx.manager->pushScene(dlgScene);
+}
+
+bool AreaEntity::checkCondition(const LevelCtx &ctx)
+{
+	// maybe require re-checking?
+	if (!m_condition.empty() && m_gen != ctx.globals.getGen())
+	{
+		m_gen = ctx.globals.getGen();
+		m_cond = ctx.globals.check(m_condition);
+	}
+
+	// returned cached result:
+	return m_cond;
 }
 
 //************************************************************************************************************************
--- src/ent/AreaEntity.h
+++ src/ent/AreaEntity.h
@@ -18,13 +18,22 @@ public:
 	explicit AreaEntity(const CL_DomNodeList &props);
 
 private:
+	// globals check:
+	mutable long m_gen;
+	mutable bool m_cond;
+
+	// properties:
 	CL_String m_area;
 	CL_String m_entry;
+	CL_String m_condition;
 
 	// virtual entity interface:
 	virtual bool update(const LevelCtx &ctx, float secs);
 	virtual bool render(const LevelCtx &ctx);
 	virtual void notify(const LevelCtx &ctx, Notify code);
+
+	// condition helpers:
+	bool checkCondition(const LevelCtx &ctx);
 };
 
 //************************************************************************************************************************
