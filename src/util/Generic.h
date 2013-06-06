4e8058fe663290e9a8371e3ed08b2849be77dbe1
SadSido <sidorian@mail.ru>
2013-06-06 20:59:43 +0400

    added generic utils

---
+++ src/util/Generic.h
@@ -0,0 +1,14 @@
+// AUTHOR: Wiatcheslav "SadSido" Sidortsov
+// ORIGIN: some very generic routines
+
+#ifndef _Generic_h_
+#define _Generic_h_
+
+//************************************************************************************************************************
+
+// number of elements in static array:
+#define COUNTOF(arr) sizeof(arr)/sizeof(arr[0])
+
+//************************************************************************************************************************
+
+#endif
